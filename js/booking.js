40ec245ab12af2a14548083327999d0b6b3b8353
Rico Pfister <contact.rico.pfister@gmail.com>
2022-03-02 16:36:20 +0100

    validation completely with javascript

--- js/booking.js
+++ js/booking.js
@@ -6,7 +6,7 @@ function userDataIds() {
 }
 
 function dummyUserData() {
-    dummyUser =  ["Herr", "Rick", "Carlton", "Bahnhofstrasse", "1", "079 123 45 67", "Zürich", "8000", "SwiftLerner9000", "rick.carlton@gmail.com", "My@Password@Is@Not@Safe@666", "My@Password@Is@Not@Safe@666", "Visa", "1234567890", "Rick Carlton", "12/26", "123", "akzeptiert"
+    dummyUser =  ["Herr", "Rick", "Carlton", "Bahnhofstrasse", "1", "079 123 45 67", "Zürich", "8000", "W3bN00b9k", "rick.carlton@gmail.com", "My@Password@Is@Not@Safe@666", "My@Password@Is@Not@Safe@666", "Visa", "1234567890", "Rick Carlton", "12/26", "123", "akzeptiert"
 ]
 }
 
@@ -15,7 +15,7 @@ document.addEventListener('keydown', function (event) {
 
         userDataIds(); dummyUserData();
 
-        for(i=0; i<userData.length; i++){
+            for(i=0; i<userData.length; i++){
 
             document.getElementById(userData[i]).value = dummyUser[i];
             document.getElementById("agb").checked = true;
@@ -41,14 +41,35 @@ document.getElementById(userData[i]).value = check;
 
 } 
 
+let valTest;
+
+// data validation
+
 function checkUserData() {
 
-    if(document.getElementById(userData[i]))
-
     userDataIds();
+
+    for(i=0; i<17; i++)
+
+    if(document.getElementById(userData[i]).value === ""){
+      document.getElementById("valID_"+userData[i]).innerHTML  = "Angabe fehlt";
+      valTest = 1;
+    }
+
+    if(valTest == 1){
+        return false;
+    }
 
     for(i=0; i<userData.length; i++){
     let check = document.getElementById(userData[i]).value;
     sessionStorage.setItem(userData[i], check);
     }    
 }
+
+// event listener klick invalid field
+
+function clearInvalid(a){
+    document.getElementById("valID_"+a).innerHTML = "";
+}
+    
+
